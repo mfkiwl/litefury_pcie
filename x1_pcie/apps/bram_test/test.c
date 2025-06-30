5f129eb05401e0f69a816f2122ed4079109df29d
Your Name <you@example.com>
2025-06-30 14:43:43 -0400

    added vinstru to bram_test

--- x1_pcie/apps/bram_test/test.c
+++ x1_pcie/apps/bram_test/test.c
@@ -21,14 +21,18 @@ int main(int argc,char** argv)
     }
     printf("FPGA_BASE_ADDRESS = 0x%10lx, virtual base_addr = %p\n", (uint64_t)FPGA_BASE_ADDRESS, base_addr);
     uint32_t *reg_ptr  = base_addr + FPGA_REG_OFFSET;
-    uint32_t* bram_ptr = base_addr + TEST_RAM_OFFSET;
+    uint32_t* bram_ptr;
     printf("FPGA_ID = 0x%08x, FPGA_VERSION = 0x%08x\n", reg_ptr[FPGA_ID], reg_ptr[FPGA_VERSION]);
 
+    uint32_t* write_data;
+    uint32_t* read_data;
+    int errors;
 
     // Test the scratch bram.
     // create test data.
-    uint32_t* write_data = malloc(TEST_RAM_SIZE);
-    uint32_t* read_data  = malloc(TEST_RAM_SIZE);
+    bram_ptr = base_addr + TEST_RAM_OFFSET;
+    write_data = malloc(TEST_RAM_SIZE);
+    read_data  = malloc(TEST_RAM_SIZE);
     for (int i=0; i<TEST_RAM_SIZE/4; i++) write_data[i] = rand();
     fprintf(stdout, "\nbram_ptr = %p\n", bram_ptr);
     // write bram
@@ -36,15 +40,36 @@ int main(int argc,char** argv)
     // read bram
     for (int i=0; i<TEST_RAM_SIZE/4; i++) read_data[i] = bram_ptr[i];
     // check bram results
-    int errors = 0;
+    errors = 0;
     for (int i=0; i<TEST_RAM_SIZE/4; i++) {
         if (read_data[i] != write_data[i]) errors++;
     }
     fprintf(stdout, "scratch bram errors = %d\n", errors);
-
-
     free(write_data);
     free(read_data);
+
+
+    // Test the VINSTRU bram.
+    // create test data.
+    bram_ptr = base_addr + VINSTRU_RAM_OFFSET;
+    write_data = malloc(VINSTRU_RAM_SIZE);
+    read_data  = malloc(VINSTRU_RAM_SIZE);
+    for (int i=0; i<VINSTRU_RAM_SIZE/4; i++) write_data[i] = rand();
+    fprintf(stdout, "\nbram_ptr = %p\n", bram_ptr);
+    // write bram
+    for (int i=0; i<VINSTRU_RAM_SIZE/4; i++) bram_ptr[i] = write_data[i];
+    // read bram
+    for (int i=0; i<VINSTRU_RAM_SIZE/4; i++) read_data[i] = bram_ptr[i];
+    // check bram results
+    errors = 0;
+    for (int i=0; i<VINSTRU_RAM_SIZE/4; i++) {
+        if (read_data[i] != write_data[i]) errors++;
+    }
+    fprintf(stdout, "vinstru bram errors = %d\n", errors);
+    free(write_data);
+    free(read_data);
+
+
     munmap(base_addr,FPGA_SIZE);
 
     return 0;
