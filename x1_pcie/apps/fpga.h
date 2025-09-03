8f400bb3e064dc739ae5eb83d3dd6a57646c5883
Your Name <you@example.com>
2025-09-03 11:38:17 -0400

    adding axi_qspi_test app

--- x1_pcie/apps/fpga.h
+++ x1_pcie/apps/fpga.h
@@ -33,3 +33,18 @@
 #define     XADC_VCCAUX             0x208
 #define     XADC_VBRAM              0x218
 
+// QSPI Configuration Flash
+#define     QSPI_OFFSET             0x00040000
+
+#define     QSPI_SRR                0x40    // 40h SRR Write N/A Software reset register
+#define     QSPI_SPICR              0x60    // 60h SPICR R/W 0x180 SPI control register
+#define     QSPI_SPISR              0x64    // 64h SPISR Read 0x0a5 SPI status register
+#define     QSPI_DTR                0x68    // 68h SPI DTR Write 0x0 SPI data transmit register. A single register or a FIFO
+#define     QSPI_DRR                0x6C    // 6Ch SPI DRR Read N/A(1) SPI data receive register. A single register or a FIFO
+#define     QSPI_SPISSR             0x70    // 70h SPISSR R/W No slave is selected 0xFFFF SPI Slave select register
+#define     QSPI_TX_OCCUPANCY       0x74    // 74h SPI Transmit FIFO Occupancy Register(2) Read 0x0 Transmit FIFO occupancy register
+#define     QSPI_RX_OCCUPANCY       0x78    // 78h SPI Receive FIFO Occupancy Register(2) Read 0x0 Receive FIFO occupancy register
+#define     QSPI_DGIER              0x1C    // 1Ch DGIER R/W 0x0 Device global interrupt enable register
+#define     QSPI_IPISR              0x20    // 20h IPISR R/TOW(3) 0x0 IP interrupt status register
+#define     QSPI_IPEIR              0x28    // 28h IPIER R/W 0x0 IP interrupt enable register
+
